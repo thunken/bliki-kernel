f14b1df395a95e5f80f211c329c419d703f04783
Luc Boruta <luc@thunken.com>
2018-08-19 00:03:20 +0200

    DEV: add getters

--- src/main/java/info/bliki/wiki/model/InterWiki.java
+++ src/main/java/info/bliki/wiki/model/InterWiki.java
@@ -1,10 +1,13 @@
 package info.bliki.wiki.model;
 
+import lombok.Getter;
+
+@Getter
 public class InterWiki {
 	final String pattern;
 	final boolean local;
 
-	InterWiki(String pattern, boolean local) {
+	InterWiki(final String pattern, final boolean local) {
 		this.pattern = pattern;
 		this.local = local;
 	}
